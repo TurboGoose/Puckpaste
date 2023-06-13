52255bcdc5b0d75eae0b43ab24352b5c6f6ebeee
TurboGoose <antonpuckich@gmail.com>
2023-06-13 14:39:55 +0300

    Implement servlet with json handling

---
+++ src/main/java/ru/turbogoose/servlets/PostServlet.java
@@ -0,0 +1,86 @@
+package ru.turbogoose.servlets;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.turbogoose.dto.*;
+import ru.turbogoose.exceptions.PostNotExistsException;
+import ru.turbogoose.services.PostService;
+
+import java.io.Writer;
+
+@WebServlet("/*")
+public class PostServlet extends HttpServlet {
+    private final PostService postService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public PostServlet(PostService postService) {
+        this.postService = postService;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        String id = extractId(req.getPathInfo());
+        try (Writer writer = resp.getWriter()) {
+            resp.setContentType("application/json");
+            try {
+                PostDto postDto = postService.getPost(id);
+                objectMapper.writeValue(writer, postDto);
+                resp.setStatus(200);
+            } catch (PostNotExistsException | JacksonException exc) {
+                objectMapper.writeValue(writer, new ErrorDto(exc.getMessage()));
+                resp.setStatus(404);
+            }
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            resp.setStatus(500);
+        }
+    }
+
+    private String extractId(String path) {
+        return path.split("/")[1];
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        try (Writer writer = resp.getWriter()) {
+            resp.setContentType("application/json");
+            try {
+                CreateDto createDto = objectMapper.readValue(req.getReader(), CreateDto.class);
+                CreatedPostDto createdPostDto = postService.createPost(createDto);
+                objectMapper.writeValue(writer, createdPostDto);
+                resp.setStatus(201);
+            } catch (JacksonException exc) {
+                objectMapper.writeValue(writer, new ErrorDto(exc.getMessage()));
+                resp.setStatus(400);
+            }
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            resp.setStatus(500);
+        }
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
+        String id = extractId(req.getPathInfo());
+        try (Writer writer = resp.getWriter()) {
+            resp.setContentType("application/json");
+            try {
+                RenewDto renewDto = objectMapper.readValue(req.getReader(), RenewDto.class);
+                RenewedPostDto renewedPostDto = postService.renewPost(id, renewDto);
+                objectMapper.writeValue(writer, renewedPostDto);
+                resp.setStatus(200);
+            } catch (PostNotExistsException | JacksonException exc) {
+                objectMapper.writeValue(writer, new ErrorDto(exc.getMessage()));
+                resp.setStatus(404);
+            }
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            resp.setStatus(500);
+        }
+    }
+}
