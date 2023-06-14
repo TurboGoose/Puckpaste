7035e2a5124ce7f3fcff8a27892818d6689ff4db
TurboGoose <antonpuckich@gmail.com>
2023-06-14 22:59:41 +0300

    Implement all DTO methods with JDBC boilerplate code

--- src/main/java/ru/turbogoose/dao/PostDao.java
+++ src/main/java/ru/turbogoose/dao/PostDao.java
@@ -9,42 +9,161 @@ public class PostDao {
     private static final String URL = "jdbc:sqlite:data/puckpaste.db";
 
     public Post getById(long id) throws PostNotFoundException {
-        try (Connection connection = DriverManager.getConnection(URL)) {
-            String sql = "SELECT * FROM posts WHERE id=?";
-            PreparedStatement ps = connection.prepareStatement(sql);
+        String sql = "SELECT * FROM posts WHERE id=?;";
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = DriverManager.getConnection(URL);
+            ps = conn.prepareStatement(sql);
             ps.setLong(1, id);
-            ResultSet result = ps.executeQuery();
-
-            if (!result.next()) {
+            rs = ps.executeQuery();
+            if (!rs.next()) {
                 throw new PostNotFoundException(id);
             }
-
-            Post post = Post.builder()
-                    .id(result.getLong("id"))
-                    .title(result.getString("title"))
-                    .description(result.getString("description"))
-                    .content(result.getString("content"))
-                    .expires(result.getTimestamp("expires").toLocalDateTime())
-                    .createdAt(result.getTimestamp("createdAt").toLocalDateTime())
-                    .lastRenewedAt(result.getTimestamp("lastRenewedAt").toLocalDateTime())
+            return Post.builder()
+                    .id(rs.getLong("id"))
+                    .title(rs.getString("title"))
+                    .description(rs.getString("description"))
+                    .content(rs.getString("content"))
+                    .expires(rs.getTimestamp("expires").toLocalDateTime())
+                    .createdAt(rs.getTimestamp("createdAt").toLocalDateTime())
+                    .lastRenewedAt(rs.getTimestamp("lastRenewedAt").toLocalDateTime())
                     .build();
 
-            return post;
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
-    public void save(Post post) {
+    public boolean save(Post post) {
         String sql = "INSERT INTO posts(title, description, content, expires, createdAt, lastRenewedAt) values (?, ?, ?, ?, ?, ?);";
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = DriverManager.getConnection(URL);
+            ps = conn.prepareStatement(sql);
+
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getDescription());
+            ps.setString(3, post.getContent());
+            ps.setTimestamp(4, Timestamp.valueOf(post.getExpires()));
+            ps.setTimestamp(5, Timestamp.valueOf(post.getCreatedAt()));
+            ps.setTimestamp(6, Timestamp.valueOf(post.getLastRenewedAt()));
+
+            int affectedRowsCount = ps.executeUpdate();
+            // TODO: return generated id
+            return affectedRowsCount > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 
     }
 
-    public void update(Post post) {
+    public boolean update(Post post) {
+        String sql = "UPDATE posts SET title=?, description=?, content=?, expires=?, createdAt=?, lastRenewedAt=? WHERE id=?;";
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = DriverManager.getConnection(URL);
+            ps = conn.prepareStatement(sql);
 
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getDescription());
+            ps.setString(3, post.getContent());
+            ps.setTimestamp(4, Timestamp.valueOf(post.getExpires()));
+            ps.setTimestamp(5, Timestamp.valueOf(post.getCreatedAt()));
+            ps.setTimestamp(6, Timestamp.valueOf(post.getLastRenewedAt()));
+            ps.setLong(7, post.getId());
+
+            int affectedRowsCount = ps.executeUpdate();
+            return affectedRowsCount == 1;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
-    public void delete(int id) {
+    public void delete(long id) {
+        String sql = "DELETE FROM posts WHERE id=?;";
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = DriverManager.getConnection(URL);
+            ps = conn.prepareStatement(sql);
 
+            ps.setLong(1, id);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
