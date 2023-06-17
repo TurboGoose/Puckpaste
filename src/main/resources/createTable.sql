bc15503237f49dffcaf3e2281ac18f0e36099b0d
TurboGoose <antonpuckich@gmail.com>
2023-06-17 19:51:17 +0300

    Add sql script for table creation

---
+++ src/main/resources/createTable.sql
@@ -0,0 +1,11 @@
+-- DROP TABLE posts;
+
+CREATE TABLE IF NOT EXISTS posts
+(
+    id          INTEGER PRIMARY KEY,
+    title       TEXT NOT NULL CHECK (length(title) <= 100),
+    description TEXT NOT NULL CHECK (length(description) <= 1500),
+    content     TEXT NOT NULL CHECK (length(content) <= 20000),
+    expires_at  TEXT NOT NULL,
+    created_at  TEXT NOT NULL
+);
