47b3804430475e9899a19b78c2297c67a72ea444
ilakonovalov <antonpuckich@gmail.com>
2023-06-28 14:26:10 +0300

    Remove redundant constructors from exceptions

--- src/main/java/ru/turbogoose/exception/MismatchException.java
+++ src/main/java/ru/turbogoose/exception/MismatchException.java
@@ -1,18 +1,7 @@
 package ru.turbogoose.exception;
 
 public class MismatchException extends PathMatcherException {
-    public MismatchException() {
-    }
-
     public MismatchException(String message) {
         super(message);
-    }
-
-    public MismatchException(Throwable cause) {
-        super(cause);
-    }
-
-    public MismatchException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
